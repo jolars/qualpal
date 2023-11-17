7abefee6685fb863d54233c58c5d7cd2c2d46807
Johan Larsson <johanlarsson@outlook.com>
2023-11-17 22:32:07 +0100

    fix: use correct name for function

--- src/qualpal/color_grid.h
+++ src/qualpal/color_grid.h
@@ -3,12 +3,12 @@
 
 namespace qualpal {
 std::vector<HSL>
-hslGrid(int n,
-        double h_start = 0,
-        double h_end = 360,
-        double s_start = 0,
-        double s_end = 1,
-        double l_start = 0,
-        double l_end = 1);
+colorGrid(int n,
+          double h_start = 0,
+          double h_end = 360,
+          double s_start = 0,
+          double s_end = 1,
+          double l_start = 0,
+          double l_end = 1);
 
 } // namespace qualpal
