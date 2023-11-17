3ccf51bddbdf58fb98341f9d4c37027034f9752a
Johan Larsson <johanlarsson@outlook.com>
2023-11-17 22:32:40 +0100

    fix: add missing definition for qualpal()

--- src/qualpal/qualpal.h
+++ src/qualpal/qualpal.h
@@ -6,4 +6,7 @@ namespace qualpal {
 std::vector<RGB>
 qualpal(const int n, const std::vector<RGB>& rgb_colors);
 
+std::vector<RGB>
+qualpal(const int n, const std::vector<std::string>& hex_colors);
+
 } // namespace qualpal
