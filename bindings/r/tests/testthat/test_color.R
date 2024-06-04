fe6dd0fca5603c2625424dd1b818dee80b098d04
Johan Larsson <johanlarsson@outlook.com>
2024-06-04 09:31:25 +0200

    chore: remove obsolote r-tests

--- bindings/r/tests/testthat/test_color.R
+++
@@ -1,9 +0,0 @@
-library(qualpalr)
-
-test_that("colors are within their respective color spaces", {
-  fit <- qualpal(n = 3)
-  expect_true(all(fit$RGB >= 0 & fit$RGB <= 1))
-  expect_true(all(fit$HSL[, 1] >= 0 & fit$HSL[, 1] <= 360))
-  expect_true(all(fit$HSL[, 2] >= 0 & fit$HSL[, 2] <= 1))
-  expect_true(all(fit$HSL[, 2] >= 0 & fit$HSL[, 2] <= 1))
-})
