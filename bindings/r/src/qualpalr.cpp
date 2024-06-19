07d5d11d330d45f8b2d83c4112691f5564c772da
Johan Larsson <johanlarsson@outlook.com>
2024-06-19 15:50:34 +0200

    feat(r): export convert_colors() function

--- bindings/r/src/qualpalr.cpp
+++ bindings/r/src/qualpalr.cpp
@@ -135,6 +135,15 @@ qualpal_cpp_colorspace(int n,
   return organize_output(selected_colors);
 }
 
+//' Convert colors between colorspaces
+//'
+//' @param colors A matrix of colors
+//' @param from The colorspace of the input colors, one of "rgb", "hsl",
+//'   "din99d", "lab", "xyz"
+//' @param to The colorspace of the output colors,
+//'   one of "rgb", "hsl", "lab", "xyz"
+//'
+//' @return The colors converted to the new colorspace
 // [[Rcpp::export]]
 Rcpp::NumericMatrix
 convert_colors(const Rcpp::NumericMatrix& colors,
