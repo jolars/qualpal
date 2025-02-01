f559eea76aad327d6c040a480e5560841d016b9a
Johan Larsson <jolars@posteo.com>
2025-02-01 22:55:07 +0100

    chore: add dependencies to flake and update lock file

---
+++ flake.nix
@@ -0,0 +1,36 @@
+{
+  description = "A basic flake with a shell";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+  inputs.systems.url = "github:nix-systems/default";
+  inputs.flake-utils = {
+    url = "github:numtide/flake-utils";
+    inputs.systems.follows = "systems";
+  };
+
+  outputs =
+    { nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          packages = with pkgs; [
+            bashInteractive
+            go-task
+            catch2_3
+            clang
+            clang-tools
+            cmake
+            doxygen
+            gcc
+            ghostscript
+            gdb
+            lcov
+            lldb
+          ];
+        };
+      }
+    );
+}
