8196aef32a87899c66f7f3f156290447e52fee92
Johan Larsson <jolars@posteo.com>
2025-07-16 21:53:28 +0200

    benchmark: add benchmark for color difference metrics

--- tests/benchmarks.cpp
+++ tests/benchmarks.cpp
@@ -32,3 +32,37 @@ TEST_CASE("Parallelization", "[!benchmark]")
     auto result = qualpal::colorDifferenceMatrix(rgb_colors);
   };
 }
+
+TEST_CASE("Color Difference Metrics", "[!benchmark]")
+{
+  std::array<double, 2> h_lim = { -360.0, 360.0 };
+  std::array<double, 2> s_lim = { 0.0, 1.0 };
+  std::array<double, 2> l_lim = { 0.0, 1.0 };
+
+  int n_points = 1000;
+
+  auto hsl_colors = qualpal::colorGrid(h_lim, s_lim, l_lim, n_points);
+
+  std::vector<qualpal::colors::Lab> lab_colors;
+  std::vector<qualpal::colors::DIN99d> din99d_colors;
+
+  for (auto color : hsl_colors) {
+    lab_colors.emplace_back(color);
+    din99d_colors.emplace_back(color);
+  }
+
+  BENCHMARK("DIN99d")
+  {
+    qualpal::colorDifferenceMatrix(din99d_colors, qualpal::metrics::DIN99d{});
+  };
+
+  BENCHMARK("CIEDE2000")
+  {
+    qualpal::colorDifferenceMatrix(lab_colors, qualpal::metrics::CIEDE2000{});
+  };
+
+  BENCHMARK("CIE76")
+  {
+    qualpal::colorDifferenceMatrix(lab_colors, qualpal::metrics::CIE76{});
+  };
+}
