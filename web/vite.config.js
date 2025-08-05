1cc23b438df4af4a8c93610136429cbdfe569b3d
Johan Larsson <jolars@posteo.com>
2025-08-05 22:57:44 +0200

    chore: autoformat

--- web/vite.config.js
+++ web/vite.config.js
@@ -1,11 +1,11 @@
-import { defineConfig } from 'vite'
-import { svelte } from '@sveltejs/vite-plugin-svelte'
+import { defineConfig } from "vite";
+import { svelte } from "@sveltejs/vite-plugin-svelte";
 
 export default defineConfig({
   plugins: [svelte()],
   server: {
     fs: {
-      allow: ['..']
-    }
-  }
-})
+      allow: [".."],
+    },
+  },
+});
