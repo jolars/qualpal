c7f3fd3670cf3ee990e7dd2402caa2f9ee9bdebb
Johan Larsson <jolars@posteo.com>
2025-08-15 09:11:38 +0200

    perf(web): compress and split css

--- web/vite.config.js
+++ web/vite.config.js
@@ -1,9 +1,21 @@
 import { defineConfig } from "vite";
 import { svelte } from "@sveltejs/vite-plugin-svelte";
 import Sitemap from "vite-plugin-sitemap";
+import viteCompression from "vite-plugin-compression";
 
 export default defineConfig({
-  plugins: [svelte(), Sitemap({ hostname: "https://qualpal.cc" })],
+  plugins: [
+    svelte(),
+    Sitemap({ hostname: "https://qualpal.cc" }),
+    viteCompression({ algorithm: "brotliCompress" }),
+  ],
+  build: {
+    target: "esnext",
+    minify: "esbuild",
+    sourcemap: false,
+    cssCodeSplit: true,
+    assetsInlineLimit: 4096,
+  },
   server: {
     fs: {
       allow: [".."],
